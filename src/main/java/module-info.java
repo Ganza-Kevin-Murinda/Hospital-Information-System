3751fb360f53f0afea0f7e13c83759e3bda49245
kevin <murindakevinmulk@gmail.com>
2025-05-09 17:12:00 +0200

    Update Configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.his.hospitalinfosystem {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires io.github.cdimascio.dotenv.java;
 
     opens com.his.hospitalinfosystem to javafx.fxml;
     exports com.his.hospitalinfosystem;
