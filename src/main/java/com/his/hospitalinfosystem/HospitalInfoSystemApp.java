12ba826f72d1a63af253789e7b313b16ffc69fda
kevin <murindakevinmulk@gmail.com>
2025-05-11 15:54:16 +0200

    Add Main App

--- src/main/java/com/his/hospitalinfosystem/HospitalInfoSystemApp.java
+++ src/main/java/com/his/hospitalinfosystem/HospitalInfoSystemApp.java
@@ -1,4 +1,34 @@
 package com.his.hospitalinfosystem;
 
-public class HospitalInformationApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class HospitalInfoSystemApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/his/hospitalinfosystem/view/patient_view.fxml"));
+        Parent root = loader.load();
+
+        // Set up the scene
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/application.css")).toExternalForm());
+
+
+        // Configure the stage
+        primaryStage.setTitle("Hospital Information System - Patient Management");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
